1178a6608db6da874a7f269f57995b2e7c8a6d3e
Frank Moreno <frankmoreno1993@gmail.com>
2021-03-13 17:07:08 -0500

    tables using linters

--- lib/src/tables.dart
+++ lib/src/tables.dart
@@ -1,7 +1,7 @@
 part of fastmath;
 
 /// Sine, Cosine, Tangent tables are for 0, 1/8, 2/8, ... 13/8 = PI/2 approx.
-const int SINE_TABLE_LEN = 14;
+const int sineTableLen = 14;
 
 /// Sine table (high bits).
 const sineTableA = <double>[
